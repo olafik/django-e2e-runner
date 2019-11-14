5ca61f54eb41e0331e1c66a5c3f50109ef5d70a4
Olaf <olafik93@gmail.com>
2019-11-14 08:12:10 +0100

    [Transaction] Rollback previous transaction before loading data
    If a load of test data was requested, first check if previous
    transaction was closed and rollback it if needed. It prevents
    an error when user's test execution was interrupted and data
    rollback didn't get requested. Moreover, it enables the user to
    skip the rollback request altogether and just expect that a next
    load request will handle the clear up.

--- django_e2e_runner/database/global_transaction.py
+++ django_e2e_runner/database/global_transaction.py
@@ -8,16 +8,24 @@ atomic = None
 
 
 class GlobalTransaction(BaseDataManager):
-    def pre_load_setup(self):
+    def pre_load_setup(self, clear_previous=True):
         connection = get_connection()
         connection.settings_dict['AUTOCOMMIT'] = False  # TODO
         connection.settings_dict["CONN_MAX_AGE"] = None
 
         global atomic
+
+        if clear_previous:
+            self.rollback()
+
         atomic = transaction.atomic(using=DEFAULT_DB_ALIAS)
         atomic.__enter__()
 
     def rollback(self):
+        connection = get_connection()
+        if not connection.in_atomic_block:
+            return
+
         global atomic
         transaction.set_rollback(True, using=DEFAULT_DB_ALIAS)
         atomic.__exit__(None, None, None)
