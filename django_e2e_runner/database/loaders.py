bd08be83f1522dcbc5acbb7596ae7821d661c7ef
Michal Wilk <michalwilk139@gmail.com>
2024-11-29 13:59:16 +0100

    added possibility for fixtures to return data

--- django_e2e_runner/database/loaders.py
+++ django_e2e_runner/database/loaders.py
@@ -4,4 +4,4 @@ from django.utils.module_loading import import_string
 class ScriptFixtureLoader(object):
     def load(self, fixture_name):
         fixture_method = import_string('tests.fixtures.{}'.format(fixture_name))
-        fixture_method()
+        return fixture_method()
